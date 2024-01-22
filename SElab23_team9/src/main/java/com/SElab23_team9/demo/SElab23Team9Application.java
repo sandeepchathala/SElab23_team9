163d44bf5407d26d66dbbcbad8487505d5d09c23
banoth4332 <156219301+banoth4332@users.noreply.github.com>
2024-01-22 19:51:00 +0530

    initial dependencies

---
+++ SElab23_team9/src/main/java/com/SElab23_team9/demo/SElab23Team9Application.java
@@ -0,0 +1,13 @@
+package com.SElab23_team9.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SElab23Team9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SElab23Team9Application.class, args);
+	}
+
+}
---
+++ SElab23_team9/src/test/java/com/SElab23_team9/demo/SElab23Team9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SElab23_team9.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SElab23Team9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
