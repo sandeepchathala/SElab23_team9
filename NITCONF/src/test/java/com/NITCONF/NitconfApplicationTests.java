7c640329439c0c9626e956371bdb4c97bf695a1b
banoth4332 <156219301+banoth4332@users.noreply.github.com>
2024-01-28 23:53:14 +0530

    new requirements and Initial dependencies

---
+++ NITCONF/src/test/java/com/NITCONF/NitconfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NITCONF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitconfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
