0edc460f6e7609d0bd1b8c9a45d23a8b6670cdb0
Banoth Balaji <Banoth Balaji@balaji>
2024-02-02 23:56:01 +0530

    Files Entity

--- nitconf/nitconf/src/main/java/com/nitconf/model/Files.java
+++ nitconf/nitconf/src/main/java/com/nitconf/model/Files.java
@@ -11,33 +11,47 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
-@Builder
-public class Files {
+public class Paper {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
-    private String author;
+    private String link;
+    //@Lob
+    //private byte[] paperdata;
     private String tags;
-    @Lob
-    private byte[] paperData;
+    private int status;
 	public long getId() {
 		return id;
 	}
 	public void setId(long id) {
 		this.id = id;
 	}
-    
-    
-
-	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getTags() {
+		return tags;
+	}
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
     
     
 }
