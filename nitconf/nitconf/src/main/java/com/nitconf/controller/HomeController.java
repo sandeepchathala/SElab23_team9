ec072d522d5b44a379690d6f666b0a0e9441eb50
Banoth Balaji <banothbalaji1729@gmail.com>
2024-02-06 01:52:36 +0530

    Home Controller for landing page

--- nitconf/nitconf/src/main/java/com/nitconf/controller/HomeController.java
+++ nitconf/nitconf/src/main/java/com/nitconf/controller/HomeController.java
@@ -1,11 +1,23 @@
 package com.nitconf.controller;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.nitconf.model.PCMember;
+import com.nitconf.model.Paper;
 
 //import ch.qos.logback.core.model.Model;
 import org.springframework.ui.Model;
@@ -13,128 +25,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@Controller
+//@Controller
+@RestController
+@RequestMapping(value="/api")
 public class HomeController {
-	PCMember f;
-	int tt;
+	Long id;
 	@Autowired
 	PCMemberrepo repo;
-	@GetMapping(value="/")
-	public String getLandingPage() {
-		return "landingpage.jsp";
-	}
-	@GetMapping(value="/Login")
-	public String getLogin() {
-		return "login.jsp";
-	}
-    @PostMapping(value="/Dashboard")
-    public String getdashboard(@RequestParam String email,@RequestParam String password,Model model) {
-    	if (repo.existsByEmailAndPassword(email,password)) {
-    		f=repo.findByEmail(email);
-    		model.addAttribute("name",f.getName());
-    	//System.out.println("arree ");
-    		return "dashboard.jsp";}
-    	else {
-    		return "Loginerror.jsp";}
-    }
-    @GetMapping(value="/Register")
-	public String getRegister() {
-		return "register.jsp";
-	}
-    @PostMapping(value="/Success")
-	public String Register(PCMember data,@RequestParam String email,@RequestParam String confirmpassword,@RequestParam String password,Model model) {
-    	if(!password.equals(confirmpassword)) {
-    		return "confirmpassworderror.jsp";
-    	}
-    	else if(repo.existsByEmail(email)) {
-    		return "emailexisterror.jsp";
-    	}
-    	else {
-    		repo.save(data);//System.out.print("success");
-    		return "success.jsp";
-    	}
-	}
-    @GetMapping(value="/Profile")
-    //import /tester/src/main/webapp/dashboard.jsp;
-    public String getprofile(Model model) {
-    	model.addAttribute("name",f.getName());
-    	model.addAttribute("phonenumber",f.getPhonenumber());
-    	model.addAttribute("email",f.getEmail());
-    	model.addAttribute("password",f.getPassword());
-    	return "profile.jsp";
-    }
-    @GetMapping(value="/EditProfile")
-    public String editprofile(Model model) {
-    	model.addAttribute("name",f.getName());
-    	model.addAttribute("phonenumber",f.getPhonenumber());
-    	model.addAttribute("email",f.getEmail());
-    	model.addAttribute("password",f.getPassword());
-    	return "editprofile.jsp";
-    }
-    @PostMapping(value="/UpdateProfile")
-    public String updateprofile(PCMember data,@RequestParam String email,@RequestParam String confirmpassword,@RequestParam String password,Model model) {
-    	if(!password.equals(confirmpassword)) {
-    		return "confirmpassworderror.jsp";
-    	}
-//    	else if(repo.existsByEmail(email)) {
-//    		return "emailexisterror.jsp";
-//    	}
-    	else {
-    		tt=f.getId();
-    		repo.deleteById(tt);
-    		f=repo.save(data);
-//    		f.setName(data.getName());
-//    		f.setPhonenumber(data.getPhonenumber());
-//    		f.setEmail(data.getEmail());
-//    		f.setPassword(data.getPassword());
-//    		model.addAttribute("name",f.getName());
-//        	model.addAttribute("phonenumber",f.getPhonenumber());
-//        	model.addAttribute("email",f.getEmail());
-//        	model.addAttribute("password",f.getPassword());
-    		System.out.println(f.getName()+" "+f.getEmail()+" "+f.getPhonenumber()+" "+data.getPassword());
-    	    return "redirect:/Profile";
-    	}
-    }
-    @GetMapping(value="/ReviewedPapers")
-    public String ReviewedPaper() {
-    	//System.out.println("why are you here....");
-    	return "reviewedpapers.jsp";
-    }
-    @GetMapping("/unreviewedpapers")
-	public String getallpapers(Model model) {
-		List<Files> all = filerepository.findAll();	
-		
-		for (Files paper : all) {
-//	        System.out.println("Paper ID: " + paper.getId());
-//	        System.out.println("Paper Title: " + paper.getTitle());
-		model.addAttribute("id",paper.getId());
-		model.addAttribute("title",paper.getTitle());
-		model.addAttribute("tags",paper.getTags());
-		model.addAttribute("status",paper.getStatus());
-		model.addAttribute("link",paper.getLink());
-//	        // Print other fields as needed
-	    }
-		//long r=1;
-		//Collection<Paper> paper=PSrepo.findAll();
-		//model.add
-		
-				//if (t.isPresent()) {
-				    //Paper paper = t.get();
-				    // Now you can safely call methods on the Paper object
-		//model.addAllAttributes(paper);
-		
-//				    model.addAttribute("id",paper.getId());
-//					model.addAttribute("title",paper.getTitle());
-//					model.addAttribute("tags",paper.getTags());
-//					model.addAttribute("status",paper.getStatus());
-//					model.addAttribute("link",paper.getLink());
-				    // ... other methods
-				//} else {
-				    // Handle the case when the Optional is empty
-				//}
+	@Autowired
+	PaperStorerepo PSrepo;
 
-		
-		
-		return "unreviewedpapers.jsp";
+//	@GetMapping("/hh")
+//	ResponseEntity<String> hello() {
+//	    return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+//	}
+	
+	@GetMapping(value="/")
+	public ModelAndView getLandingPage() {
+		ModelAndView mav = new ModelAndView("landing.jsp");
+		return mav;
 	}
+    
 }
