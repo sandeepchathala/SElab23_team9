7a59a0b020a88451c9235457594017699be763f4
sandeepchathala <sandeepchathala007@gmail.com>
2024-02-07 01:18:10 +0530

    edited PCMember entity

--- nitconf/nitconf/src/main/java/com/nitconf/controller/PCMemberrepo.java
+++ nitconf/nitconf/src/main/java/com/nitconf/controller/PCMemberrepo.java
@@ -3,13 +3,19 @@ package com.nitconf.controller;
 //import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 //import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.nitconf.model.PCMember;
 
-public interface PCMemberrepo extends JpaRepository<PCMember ,Integer>{
+public interface PCMemberrepo extends JpaRepository<PCMember ,Long>{
 	boolean existsByEmailAndPassword(String email, String password);
 	PCMember findByEmail(String email);
 	boolean existsByEmail(String email);
+	
+	@Modifying
+	@Query("UPDATE PCMember SET name=:name,email=:email,password=:password WHERE id=:id")
+	void setvalues(@Param("id") long id,@Param("name") String name,@Param("email") String email,@Param("password") String password);
 }
