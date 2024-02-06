ec930e303c29064156a4389249764442b24f7ea7
mouliyedla <mouliyedla789@gmail.com>
2024-02-07 00:55:52 +0530

    modify paper controller

--- nitconf/nitconf/src/main/java/com/nitconf/controller/PaperController.java
+++ nitconf/nitconf/src/main/java/com/nitconf/controller/PaperController.java
@@ -1,39 +1,46 @@
-
-
-
 package com.nitconf.controller;
 
-import java.io.IOException;
+//import java.util.Date;
+import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.nitconf.service.FileService;
+import com.nitconf.model.Paper;
+//import com.nitconf.services.PaperStoreService;
+
 
 @RestController
+@RequestMapping(value="/Paper")
 public class PaperController {
 	
 	@Autowired
-	private FileService filesService;
+	PaperStorerepo PSrepo;
 	
-	@PostMapping("/Uploadpaper")
-	public ResponseEntity<String> storeFilesInDB(@RequestParam String author,@RequestParam("file") MultipartFile file) throws IOException {
-		String response = filesService.storeFile(file,author);
-		System.out.println("adfidfndif");
-		return ResponseEntity.status(HttpStatus.OK).body(response);
-	}
-	
-	
-
-			
-			
-	
-	
-
+    @PostMapping("/StorePaper")
+    public ModelAndView storepaper( @RequestParam String title,
+            @RequestParam String tags,
+            @RequestParam String link) {
+    	Paper paper = new Paper();
+    	paper.setTitle(title);
+        paper.setTags(tags);
+        paper.setLink(link);
+        paper.setStatus(0);
+        paper.setUploadeddate(LocalDate.now());
+    	PSrepo.save(paper);
+    	ModelAndView m= new ModelAndView("success.jsp");
+    	return m;
+    }
+    @GetMapping("/upload")
+    public ModelAndView upload() {
+    	ModelAndView mav = new ModelAndView("upload.jsp");
+    	return mav;
+    }
 }
