b39f2fb095d4cbc887095d6c1a377b2cfc3e20a2
mouliyedla <115372676+mouliyedla@users.noreply.github.com>
2024-03-12 12:18:49 +0530

    Update PapersControllerTest.java

--- nitconf/nitconf/src/test/java/com/nitconf/controllertest/PapersControllerTest.java
+++ nitconf/nitconf/src/test/java/com/nitconf/controllertest/PapersControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import com.nitconf.controller.PapersController;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(PapersController.class)
