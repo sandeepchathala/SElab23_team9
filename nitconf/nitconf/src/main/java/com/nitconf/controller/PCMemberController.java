49a2787070bd4f37d8db238f599b6cdb000a3058
sandeepchathala <156419640+sandeepchathala@users.noreply.github.com>
2024-03-17 11:27:39 +0530

    Update PCMemberController.java

--- nitconf/nitconf/src/main/java/com/nitconf/controller/PCMemberController.java
+++ nitconf/nitconf/src/main/java/com/nitconf/controller/PCMemberController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.stereotype.Controller;
 import com.nitconf.model.PCMember;
 
 import jakarta.servlet.http.HttpSession;
