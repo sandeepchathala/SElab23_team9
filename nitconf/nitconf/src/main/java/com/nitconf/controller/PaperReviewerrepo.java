ef08dbe53871a9c140d89f5970c37a431d179f10
sandeepchathala <156419640+sandeepchathala@users.noreply.github.com>
2024-03-18 21:49:53 +0530

    Update PaperReviewerrepo.java added queries to retrieve papers, reviewers

--- nitconf/nitconf/src/main/java/com/nitconf/controller/PaperReviewerrepo.java
+++ nitconf/nitconf/src/main/java/com/nitconf/controller/PaperReviewerrepo.java
@@ -1,9 +1,27 @@
 package com.nitconf.controller;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 import com.nitconf.model.PaperReviewer;
 
 public interface PaperReviewerrepo extends JpaRepository<PaperReviewer,Long>{
-
+	@Query("SELECT t,p,r FROM PaperReviewer t "+
+	        "JOIN Paper p ON p.id=t.paper_id "+
+		    "JOIN Reviewer r ON r.id=t.reviewer_id "+
+	        "WHERE t.paper_id =:paper_id AND p.status = 3")
+    public List<Object[]> getreview_accept(Long paper_id);
+    
+	@Query("SELECT t,p,r FROM PaperReviewer t "+
+	        "JOIN Paper p ON p.id=t.paper_id "+
+		    "JOIN Reviewer r ON r.id=t.reviewer_id "+
+	        "WHERE t.paper_id =:paper_id AND p.status = 4")
+    public List<Object[]> getreview_reject(Long paper_id);
+    
+	@Query("SELECT t,r FROM PaperReviewer t "+
+	        "JOIN Paper p ON p.id=t.paper_id "+
+		    "JOIN Reviewer r ON r.id=t.reviewer_id "+
+	        "WHERE t.paper_id =:paper_id AND p.status = 2")
+    public List<Object[]> showreview(Long paper_id);
 }
