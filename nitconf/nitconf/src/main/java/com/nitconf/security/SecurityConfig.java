a06b5d860f98f9b09f3448b521de6fa9e5acb359
sandeepchathala <156419640+sandeepchathala@users.noreply.github.com>
2024-03-18 18:47:46 +0530

    Create SecurityConfig.java for secure login

---
+++ nitconf/nitconf/src/main/java/com/nitconf/security/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.nitconf.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    
+	@Bean 
+	public PasswordEncoder passwordEncoder()     
+	{   
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public UserDetailsService getDetailsService() {
+		return new SecurityUserDetailsService();
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationprovider()
+	{
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		
+		provider.setUserDetailsService(getDetailsService());
+		provider.setPasswordEncoder(passwordEncoder());
+		
+		return provider;	
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/*").permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin((form) -> form
+				.loginPage("/login")
+				.permitAll()
+				.defaultSuccessUrl("/api/pcmember/Dashboard",false)
+				.failureUrl("/login")
+				
+			)
+      /* following later may be used */
+//			.exceptionHandling(exceptionHandling -> exceptionHandling
+//			.accessDeniedHandler(accessDeniedHandler))
+			.logout((logout) -> logout.permitAll());
+		return http.build();
+	}
+	
+}
