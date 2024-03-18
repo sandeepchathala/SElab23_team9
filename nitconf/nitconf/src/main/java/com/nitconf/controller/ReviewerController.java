42de9946eef78ade13dfd18bf2a7b7ef5bf13132
sandeepchathala <156419640+sandeepchathala@users.noreply.github.com>
2024-03-19 02:42:40 +0530

    Updated ReviewerController.java added assign additional reviewer feature

--- nitconf/nitconf/src/main/java/com/nitconf/controller/ReviewerController.java
+++ nitconf/nitconf/src/main/java/com/nitconf/controller/ReviewerController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
